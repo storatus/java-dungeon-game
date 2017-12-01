c6f63abeef8e3f7775ee2286a1167064242db47e
Bob Currie <rfc31@bath.ac.uk>
2017-12-01 11:21:59 +0000

    Add files via upload

---
+++ src/Coordinates.java
@@ -0,0 +1,25 @@
+public class Coordinates {
+    private int x_Coordinate;
+    private int y_Coordinate;
+
+    public Coordinates(int x_Coordinate, int y_Coordinate) {
+        this.x_Coordinate = x_Coordinate;
+        this.y_Coordinate = y_Coordinate;
+    }
+
+    public int getX_Coordinate() {
+        return x_Coordinate;
+    }
+
+    public void setX_Coordinate(int x_Coordinate) {
+        this.x_Coordinate = x_Coordinate;
+    }
+
+    public int getY_Coordinate() {
+        return y_Coordinate;
+    }
+
+    public void setY_Coordinate(int y_Coordinate) {
+        this.y_Coordinate = y_Coordinate;
+    }
+}
