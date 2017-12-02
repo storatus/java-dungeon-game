8f11af5e3b42ebfcf2a363051982d0963a327853
Bob Currie <rfc31@bath.ac.uk>
2017-12-02 19:01:03 +0000

    Gold Scoring System and JUnit tests
    Gold Scoring System meeting FR2b, FR2hii, User Story US004 with associated JUnit testing

--- src/PlayerScoreTest.java
+++ src/PlayerScoreTest.java
@@ -1,34 +1,52 @@
 //package com.SWEngCW2;
 
+/**
+ * JUnit tests for Gold scoring system for
+ * Multi-Player Adventure Game SWEngCW02. Testing
+ * PlayerScore.java addToGoldScore(),  reduceGoldScore()
+ * and showGoldScore() methods
+ *
+ * @version 1.0
+ * @release 12/11/2017 Addressing Function Requirement FR2b, FR2hii, User Story
+ *          US004
+ * @See PlayerScore.java
+ */
+
+import com.SWEngCW2.PlayerScore;
+
 import static org.junit.Assert.*;
 
 public class PlayerScoreTest {
 
+    // jUnit test getGoldScore() method
     @org.junit.Test
     public void getGoldScore() throws Exception {
-        PlayerScore sc = new PlayerScore("Sanjeev",
+        PlayerScore bc = new PlayerScore("Bob",
                 150);
-        int currentScore = sc.getGoldScore();
+        int currentScore = bc.getGoldScore();
         assertEquals(150, currentScore, 0);
     }
 
+    // jUnit test addTotGoldScore() method
     @org.junit.Test
     public void addToGoldScore() throws Exception {
-        PlayerScore sc = new PlayerScore("Sanjeev",
+        PlayerScore bc = new PlayerScore("Bob",
                 0);
-        int currentScore = sc.addToGoldScore(400);
+        int currentScore = bc.addToGoldScore(400);
         assertEquals(400, currentScore, 0);
     }
 
+    // jUnit test reduceGoldScore() method
     @org.junit.Test
     public void reduceGoldScore() throws Exception {
-        PlayerScore sc = new PlayerScore("Sanjeev",
+        PlayerScore bc = new PlayerScore("Bob",
                 1000);
-        int currentScore = sc.reduceGoldScore(400);
+        int currentScore = bc.reduceGoldScore(400);
         assertEquals(600, currentScore, 0);
 
     }
 
+    jUnit test showGoldScore() method
     @org.junit.Test
     public void showGoldScore() throws Exception {
         PlayerScore sc = new PlayerScore("Sanjeev",
@@ -39,28 +57,3 @@ public class PlayerScoreTest {
 
 
 }
-
-
-//Running tests
-
-//PlayerScore bc = new PlayerScore("Bob", 0);
-//
-//System.out.println("Initial gold score: " + bc.showGoldScore());
-//
-//bc.addToGoldScore(200);
-//
-//System.out.println("Current gold score : " + bc.showGoldScore());
-//bc.reduceGoldScore(100);
-//System.out.println("Current gold score : " + bc.showGoldScore());
-//Room entranceHall = new Room(8, 10);
-////entranceHall.printRoom();
-//System.out.println();
-//
-//entranceHall.setRoomGridLocation(3, 4,
-//true);
-//System.out.println(entranceHall.getRoomGridLocation(3, 4));
-//entranceHall.printRoom();
-//System.out.println();
-//
-//Room armoury = new Room (6, 14);
-//armoury.printRoom();
