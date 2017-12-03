fe26456f0794b1196922c280ccfd7c5b4815fd71
Bob Currie <rfc31@bath.ac.uk>
2017-12-03 17:48:59 +0000

    Player Class and associated JUnit tests
    Player Class and Associated JUnit Tests, Addressing Addressing  Functional Requirement FR1a, FR2f, User Story US005, US006, US007

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -1,60 +1,97 @@
+
+/**
+ * JUnit tests for Player class  for
+ * Multi-Player Adventure Game . Testing
+ * Player.java moveRight(),  moveLeft()
+ * moveUp() and moveDown() methods
+ *
+ * @version 1.0
+ * @release 21/11/2017 Addressing  Functional
+ *      Requirement FR1a, FR2f, User Story US005,
+ *      US006, US007
+ *
+ *  @See PlayerScore.java, Player.java, PlayerTest.java,
+ * MainPlayerMoveRoomChange.java
+ */
+
+
 import org.junit.internal.runners.statements.Fail;
 
 import static org.junit.Assert.*;
 
 public class PlayerTest {
-	@org.junit.Test
-	public void moveRight() throws Exception {
-		Room entrancehall = new Room("Entrancehall", 8, 10);
-		Player bob = new Player(entrancehall, 5, 5);
-		int rightLocation = bob.moveRight();
-		assertEquals(6, rightLocation, 0);
 
-		// test player does not move beyond wall boundary
-		Player bc = new Player(entrancehall, 7, 9);
-		int rightLocation2 = bc.moveRight();
-		assertEquals(9, rightLocation2, 0);
-	}
+    // test player rightwards movement
+    @org.junit.Test
+    public void moveRight() throws Exception {
+        Room entrancehall =
+                new Room("Entrancehall", 8, 10);
+        Player bob = new Player(entrancehall,
+                5, 5);
+        int rightLocation = bob.moveRight();
+        assertEquals(6, rightLocation, 0);
 
-	@org.junit.Test
-	public void moveLeft() throws Exception {
-		Room entrancehall = new Room("Entrancehall", 8, 10);
-		Player bob = new Player(entrancehall, 5, 5);
-		int leftLocation = bob.moveLeft();
-		assertEquals(4, leftLocation, 0);
+        // test player does not move beyond wall boundary
+        Player bc =
+                new Player(entrancehall,
+                        7, 9);
+        int rightLocation2 = bc.moveRight();
+        assertEquals(9, rightLocation2, 0);
+    }
 
-		// test player does not move beyond wall boundary
-		Player bc = new Player(entrancehall, 0, 0);
-		int leftLocation2 = bc.moveLeft();
-		assertEquals(0, leftLocation2, 0);
+    // test player left movement
+    @org.junit.Test
+    public void moveLeft() throws Exception {
+        Room entrancehall =
+                new Room("Entrancehall", 8, 10);
+        Player bob = new Player(entrancehall,
+                5, 5);
+        int leftLocation = bob.moveLeft();
+        assertEquals(4, leftLocation, 0);
 
-	}
+        // test player does not move beyond wall boundary
+        Player bc =
+                new Player(entrancehall, 0, 0);
+        int leftLocation2 = bc.moveLeft();
+        assertEquals(0, leftLocation2, 0);
 
-	@org.junit.Test
-	public void moveDown() throws Exception {
-		Room entrancehall = new Room("Entrancehall", 8, 10);
-		Player bob = new Player(entrancehall, 5, 5);
-		int downLocation = bob.moveDown();
-		assertEquals(6, downLocation, 0);
+    }
 
-		// test player does not move beyond wall boundary
-		Player bc = new Player(entrancehall, 7, 9);
-		int downLocation2 = bc.moveDown();
-		assertEquals(7, downLocation2, 0);
+    //test player upward movement
+    @org.junit.Test
+    public void moveDown() throws Exception {
+        Room entrancehall =
+                new Room("Entrancehall", 8, 10);
+        Player bob = new Player(entrancehall,
+                5, 5);
+        int downLocation = bob.moveDown();
+        assertEquals(6, downLocation, 0);
 
-	}
+        // test player does not move beyond wall boundary
+        Player bc =
+                new Player(entrancehall,
+                        7, 9);
+        int downLocation2 = bc.moveDown();
+        assertEquals(7, downLocation2, 0);
 
-	@org.junit.Test
-	public void moveUp() throws Exception {
-		Room entrancehall = new Room("Entrancehall", 8, 10);
-		Player bob = new Player(entrancehall, 5, 5);
-		int upLocation = bob.moveUp();
-		assertEquals(4, upLocation, 0);
+    }
 
-		// test player does not move beyond wall boundary
-		Player bc = new Player(entrancehall, 0, 0);
-		int upLocation2 = bc.moveUp();
-		assertEquals(0, upLocation2, 0);
-	}
+    // test player upward movement
+    @org.junit.Test
+    public void moveUp() throws Exception {
+        Room entrancehall =
+                new Room("Entrancehall", 8, 10);
+        Player bob = new Player(entrancehall,
+                5, 5);
+        int upLocation = bob.moveUp();
+        assertEquals(4, upLocation, 0);
+
+        // test player does not move beyond wall boundary
+        Player bc =
+                new Player(entrancehall,
+                        0, 0);
+        int upLocation2 = bc.moveUp();
+        assertEquals(0, upLocation2, 0);
+    }
 
 }
